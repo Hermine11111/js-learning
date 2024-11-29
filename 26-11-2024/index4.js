951c3991a51351861560f8ba98aca7de7edffaa4
Hermine <hermineharutyunyan429@gmail.com>
2024-11-29 15:21:43 +0400

    Right-angled triangle pattern. Done.

---
+++ 26-11-2024/index4.js
@@ -0,0 +1,9 @@
+// Print a right-angled triangle pattern using 'for loop'.The pattern should have 10 rows.
+
+let rows = 10;
+let stars = '';
+
+for(let i = 1; i <= rows; i++) {
+    stars+= '*';
+    console.log(stars) ;
+};
